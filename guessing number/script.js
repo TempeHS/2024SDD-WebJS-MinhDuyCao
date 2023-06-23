652bb21c855dd36373306c9426240aa898485c59
justbill24 <minh.cao508@education.nsw.gov.au>
2023-06-23 14:42:42 +1000

    commit change

---
+++ guessing number/script.js
@@ -0,0 +1,50 @@
+var randomNumber = 0;
+var playerGuess = 0;
+var computerScore = 0;
+var playerScore = 0;
+var playerTip = 0;
+var countDownFrom = 10;
+var currentSecond = 11;
+var timerId = 0;
+
+function gameLoop() {
+    generateRandomNumber();
+    equalityCheck();
+    updateHtmlContent();
+    //secondCountDown ();
+}
+
+
+function generateRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+}
+
+
+function updateHtmlContent() {
+    document.getElementById("playerScoreContent").innerHTML = playerScore;
+    document.getElementById("computerScoreContent").innerHTML = computerScore;
+    document.getElementById("tipContent").innerHTML = playerTip;
+}
+
+
+function equalityCheck() {
+    playerGuess = document.getElementById("playerGuessInput").value;
+    playerGuess = Number(playerGuess);
+    if (playerGuess === randomNumber) {
+        currentSecond = 11;
+        playerScore = playerScore + 1;
+        playerTip = "correct, click to play again";
+        document.getElementById("playButton").value = "play now";
+    } else {
+        document.getElementById("playButton").value = "guess again";
+        if (playerGuess < randomNumber) {
+            playerTip = "it's higher than" + playerGuess;
+        }
+        if (playerGuess > randomNumber) {
+            playerTip = "it's lower than " + playerGuess;
+        }
+        if (isNaN(playerGuess)){
+            playerTip = "enter a number between 1 and 10";
+        }
+    }
+}
